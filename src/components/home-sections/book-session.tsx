43eebb04d1a7238c8a5337ceb821e5e2583f5f9b
ahmad wehbe <ahmdbwhbe@gmail.com>
2023-02-25 13:49:02 +0200

    adding background to booking section

--- src/components/home-sections/book-session.tsx
+++ src/components/home-sections/book-session.tsx
@@ -1,16 +1,33 @@
 import Script from "next/script";
 import styles from "../../assets/calendly.module.css";
+import Image from "next/image";
 
 export default function BookSection() {
   return (
-    <div className="bg-primary py-5 flex flex-col gap-5 px-5">
+    <div className="bg-primary py-5 flex flex-col gap-5 px-5 relative">
+      <div className="invisible absolute flex justify-between align-middle top-0 left-0 w-full h-full z-0 lg:visible">
+        <Image
+          className="object-contain"
+          src="/images/booking/left.png"
+          alt="background"
+          width={90}
+          height={400}
+        />
+        <Image
+          className="object-contain"
+          src="/images/booking/right.png"
+          alt="background"
+          width={90}
+          height={400}
+        />
+      </div>
       <h1 className="title text-white text-center">Book Your Session</h1>
       <p className="paragraph text-white text-center mini:text-lg lg:text-xl xl:text-2xl">
         Take the first step to change your life.
       </p>
       <div
         className={
-          "calendly-inline-widget w-full overflow-hidden rounded-xl " +
+          "z-10 calendly-inline-widget w-full overflow-hidden rounded-xl " +
           styles.customHeights
         }
         data-url="https://calendly.com/whbeagncy/test?text_color=3c4856&primary_color=5c96cc"
