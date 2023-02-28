add76057a540f8bfdb58cb627c1be40499b7b99b
ahmad wehbe <ahmdbwhbe@gmail.com>
2023-02-28 16:09:40 +0200

    edit to FAQ section

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -17,7 +17,7 @@ export default function Question({
         height={100}
       />
       <div className="w-full flex flex-col">
-        <p className="text-secondary font-extrabold font-inter text-start text-xl">
+        <p className="text-secondary font-extrabold font-inter text-start md:text-xl">
           {question}
         </p>
         <p className="text-gray-500 font-inter text-base">{answer}</p>
